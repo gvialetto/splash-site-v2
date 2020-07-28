47bdf1f66fb586a08bfb7e33a1733270700f2403
Gianni Vialetto <g@vialetto.me>
2020-07-28 22:59:45 +0100

    Add CSS colors used dynamically to purgeCSS whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,25 @@
 module.exports = {
-  purge: ["./components/*.tsx", "./pages/**/*.tsx"],
+  purge: {
+    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+    options: {
+      // We generate those dynamically, so we need to whitelist
+      // them here.
+      whitelist: [
+        "bg-red-500",
+        "bg-blue-500",
+        "bg-green-600",
+        "bg-purple-500",
+        "bg-teal-400",
+        "bg-orange-400",
+        "fg-red-500",
+        "fg-blue-500",
+        "fg-green-600",
+        "fg-purple-500",
+        "fg-teal-400",
+        "fg-orange-400",
+      ],
+    }
+  },
   theme: {},
   variants: {},
   plugins: [],
